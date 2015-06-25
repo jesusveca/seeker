5a69bce50afdb8c33847267880da4c984543a6a2
Oscar Chujutalli <oscarchs@bitbucket.com>
2015-06-25 01:33:22 -0500

    mejorando la interfaz

--- hello.py
+++ hello.py
@@ -2,20 +2,31 @@
 from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask import request, flash
 
+ 
+app = Flask(__name__) 
+ 
+app.secret_key = 'development key'
 
-app= Flask(__name__)
 
 
 @app.route("/")
 def hello():
-   return render_template('bootstrapexample.html')
+   return render_template('index.html')
 
 @app.route("/controller")
 def controller():
    return render_template('index.html',list = ['julio','juan'])
 
+@app.route('/search', methods = ['POST'])
+def search():
 
+#con las Keywords obtendremos una estructura que contiene los resultados de busqueda
+
+   keywords = request.form['keywords']
+
+   return render_template('search.html',keywords = keywords.split()	,method = 'POST')
 
 
 Bootstrap(app)
