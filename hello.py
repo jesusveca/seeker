b273425a9d1d2085bb5f7312e2f1d1346dd5238b
Oscar Chujutalli <oscarchs@bitbucket.com>
2015-06-25 20:26:17 -0500

    holapokebola

--- hello.py
+++ hello.py
@@ -3,13 +3,16 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
 from flask import request, flash
-
+from flask.ext.sqlalchemy import SQLAlchemy
  
+
 app = Flask(__name__) 
  
 app.secret_key = 'development key'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/new'
 
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
@@ -25,6 +28,12 @@ def search():
 #con las Keywords obtendremos una estructura que contiene los resultados de busqueda
 
    keywords = request.form['keywords']
+
+   
+   result = []
+
+   result = keywords
+
 
    return render_template('search.html',keywords = keywords.split()	,method = 'POST')
 
