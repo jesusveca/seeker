baa146cf93184a873bd10f98b6966f074713f1c1
Oscar Chujutalli <oscarchs@bitbucket.com>
2015-06-26 16:30:58 -0500

    interfaz 95%

--- hello.py
+++ hello.py
@@ -3,15 +3,21 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
 from flask import request, flash
-
+import pre_process
  
 
 app = Flask(__name__) 
  
 app.secret_key = 'development key'
 
-result_global = []
 
+
+query_global = ""
+result_global = []
+result_total_len = 0
+
+processlogic = pre_process.PreProcessManager()
+processlogic.read_all_data_from_files()
 
 
 @app.route("/")
@@ -31,15 +37,28 @@ def ejemplo(lista):
 @app.route("/search", methods = ['POST'])
 def search():
 #con las Keywords obtendremos una estructura que contiene los resultados de busqueda
+   global query_global
    global result_global
-   result_global = request.form['keywords']
-   return render_template('search.html',keywords = result_global.split(),method = 'POST')
+   global result_total_len
+
+   query_global = request.form['keywords']
+
+   result_global = processlogic.process_query(query_global) #lista de tuplas
+
+   result_total_len = int(len(result_global)/20) + 2
+
+   result_temp = result_global[:20]
+
+   return render_template('search.html',keywords = query_global, result = result_temp , len_result = result_total_len,method = 'POST')
 
 #@app.route("/search")
-@app.route("/search/<page_id>")
+@app.route("/search/<int:page_id>")
 def pagine(page_id):
 
-   return render_template('search.html', keywords = ejemplo(result_global))
+   index = page_id*20
+   result_temp = result_global[index-20:index]
+
+   return render_template('search.html', keywords = query_global, result = result_temp, len_result = result_total_len)	
 
 
 
