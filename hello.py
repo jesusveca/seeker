895df190e1c8258944be8bc40e1ee20564ee81bc
Oscar Chujutalli <oscarchs@bitbucket.com>
2015-07-05 20:07:46 -0500

    importando bootstrap localmente

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 from flask import Flask
 from flask import render_template
-from flask_bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap
 from flask import request, flash
 import pre_process
 import datetime
@@ -61,6 +61,8 @@ def search():
 
    result_total_len = int(len(result_global)/20) + 2
 
+   result_temp = []
+
    if from_date!="" and to_date!="":  #ambas fechas
       both_date_set = True
       from_date = datetime.datetime.strptime(from_date[2:],"%y-%m-%d").date()
@@ -72,17 +74,17 @@ def search():
         temp = processlogic.json_obj.select_data(x[0])  # date  = datetime.date(2013, 5, 16)
         if temp[0][0] < to_date and temp[0][0] > from_date:     
             result_final.append(temp[0])
+            result_temp.append(x)
             count += 1
    else:
       both_date_set = False
       result_temp = result_global[:20]
-      result_final = []
       for x in result_temp:
          temp = processlogic.json_obj.select_data(x[0])
          result_final.append(temp[0])
  
    #print (result_final)
-   return render_template('search.html',keywords = query_global, result = result_final , from_date = from_date, to_date = to_date, len_result = result_total_len,method = 'POST')
+   return render_template('search.html',keywords = query_global, result = result_final, similarities = result_temp, from_date = from_date, to_date = to_date, len_result = result_total_len,method = 'POST')
 
 #@app.route("/search")
 @app.route("/search/<int:page_id>")
@@ -90,6 +92,8 @@ def pagine(page_id):
 
    index = page_id*20
    result_final = []
+   result_temp = []
+
    if both_date_set == True:
       result_temp = result_global[index-20:]
       count = 0
@@ -99,6 +103,7 @@ def pagine(page_id):
          temp = processlogic.json_obj.select_data(x[0])  # date  = datetime.date(2013, 5, 16)
          if temp[0][0] < to_date and temp[0][0] > from_date:     
             result_final.append(temp[0])
+            result_temp.append(x)
             count += 1
    else:
       result_temp = result_global[index-20:index]      
@@ -106,7 +111,7 @@ def pagine(page_id):
          temp = processlogic.json_obj.select_data(x[0])
          result_final.append(temp[0])
 
-   return render_template('search.html', keywords = query_global, result = result_final, from_date = from_date, to_date = to_date, len_result = result_total_len)	
+   return render_template('search.html', keywords = query_global, result = result_final, similarities = result_temp, from_date = from_date, to_date = to_date, len_result = result_total_len)	
 
 Bootstrap(app)
 if __name__ == "__main__":
